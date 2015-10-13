d575c89e82129493035fc087a31fb5e9010e0bc2
Sangeon Park <pse961202@gmail.com>
2015-10-13 14:11:44 +0900

    HW 2-3 fix (absolute value)

--- homeworks/hw2-skeleton/hw2-3.rkt
+++ homeworks/hw2-skeleton/hw2-3.rkt
@@ -3,5 +3,6 @@
 (provide iter)
 
 (define (iter n f)
-  (if (<= n 0) (lambda (x) x)
-      (lambda (x) (f ((iter (- n 1) f) x)))))
+  (if (= n 0) (lambda (x) x)
+      (if (> n 0) (lambda (x) (f ((iter (- n 1) f) x)))
+        (lambda (x) (f ((iter (+ n 1) f) x))))))
