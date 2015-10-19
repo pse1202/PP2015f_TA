36dc00684e42e870f41042c32fc3a755d190a6c5
Sangeon Park <pse961202@gmail.com>
2015-10-19 20:44:12 +0900

    HW 3-1,2 done

--- homeworks/hw3-skeleton/hw3-1.rkt
+++ homeworks/hw3-skeleton/hw3-1.rkt
@@ -1,35 +1,35 @@
-# lang racket
+#lang racket
 ; not for execution
 ; write a type of each expression
 
-(define (sigma lower upper)
-  (lambda (f)
-    (define (loop n)
-      (if (> n upper) 0
-          (+ (f n) (loop (+ n 1)))))
-    (loop lower)
-    )
+(define (sigma #|int int -> ((int -> int) -> int)|# lower #|int|# upper #|int|#)
+  (lambda (f) #|int -> int|#
+    (define (loop #|int -> int|# n #|int|#)
+      (if (> n upper) #|bool|# 0
+          (+ (f n)#|int|# (loop (+ n 1))#|int|#)))#|int|#
+    (loop lower)#|int|#
+    )#|(int -> int) -> int|#
   )
 
-(define (generic-sum lower upper f larger base op inc)
-  (if (larger lower upper) base
-      (op (f lower)
-          (generic-sum (inc lower) upper f larger base op inc))
-      )
+(define (generic-sum #|'a 'b ('a->'c) ('a 'b -> bool) 'd ('c 'd -> 'd) ('a -> 'a) -> 'd|# lower #|'a|# upper #|'b|# f #|'a -> 'c|# larger #|'a 'b -> bool|# base #|'d|# op #|'c 'd -> 'd|# inc #|'a->'a|#)
+  (if (larger lower upper) #|bool|# base 
+      (op (f lower) #|'c|#
+          (generic-sum (inc lower) #|'a|# upper f larger base op inc) #|'d|#) #|'d|#
+      )#|'d|#
   )
 
-(define (map f l)
-  (if (null? l) '()
-      (cons (f (car l)) (map f (cdr l)))
-      )
+(define (map #|('a -> 'b) 'a list -> 'b list|# f #|'a -> 'b|# l #|'a list|#)
+  (if (null? l) #|bool|# '() #|'b list|#
+      (cons (f (car l)#|'a|#)#|'b|# (map f (cdr l))#|'b list|#)#|'b list|#
+      )#|'b list|#
   )
 
-(define (reduce l op init)
-  (if (null? l) init
-      (op (car l) (reduce (cdr l) op init))
-      )
+(define (reduce #|'a list ('a 'b -> 'b) 'b -> 'b|# l #|'a list|# op #|'a 'b -> 'b|# init #|'b|#)
+  (if (null? l) #|bool|# init #|'b|# 
+      (op (car l) #|'a|# (reduce (cdr l) #|'a list|# op init) #|'b|#) #|'b|#
+      ) #|'b|#
   )
 
-(define (map-reduce f l op init)
-  (reduce (map f l) op init)
+(define (map-reduce #|('a -> 'b) 'a list ('b 'c -> 'c) 'c -> 'c|# f #|'a -> 'b|# l #|'a list|# op #|'b 'c -> 'c|# init #|'c|#)
+  (reduce #|'b list ('b 'c -> 'c) 'c -> 'c|# (map  #|('a -> 'b) 'a list -> 'b list|# f l) op  init) #|'c|#
   )
--- homeworks/hw3-skeleton/hw3-2.rkt
+++ homeworks/hw3-skeleton/hw3-2.rkt
@@ -5,5 +5,31 @@
 (provide maze-check)
 
 (define (maze-check maze start end)
-  'TODO
+  (define oldset empty-set)
+  (define newset (add-element start empty-set))
+  (define oldlist null)
+  (define newlist (can-enter start maze))
+  (let loop()
+    (set! oldset newset)
+    (set! newset (addlist newlist oldset))
+    (set! oldlist newlist)
+    (set! newlist (remove-duplicates (can-enter-list oldlist maze) same-room? ))
+    (when (not (is-sameset? oldset newset)) (loop)))
+  (if (is-member? end newset) #t #f)
   )
+
+(define (addlist roomlist roomset)
+  (if (null? roomlist) roomset
+      (if (is-member? (car roomlist) roomset)
+      (addlist (cdr roomlist)  roomset)
+      (addlist (cdr roomlist) (add-element (car roomlist) roomset))
+      )))
+
+(define (is-sameset? seta setb)
+  (and (is-subset? seta setb) (is-subset? setb seta)))
+
+
+(define (can-enter-list roomlist maze)
+  (if (null? roomlist) null
+      (append (can-enter (car roomlist) maze) (can-enter-list (cdr roomlist) maze))))
+  
