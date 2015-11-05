f0f71b09d20e4d716d2a69f4be4dc0b83d54989b
Sangeon Park <pse961202@gmail.com>
2015-11-06 01:27:22 +0900

    HW3 50%

--- homeworks/hw5-skeleton/hw5-3.rkt
+++ homeworks/hw5-skeleton/hw5-3.rkt
@@ -4,24 +4,25 @@
 (require "hw5-2.rkt")
 
 (provide is-empty? fst rest length nth-elmt map reduce)
+(define unit '())
 
 (define (is-empty? l)
   (equal? l empty))
 
 (define (fst l)
-  (case-list (lambda () '()) (lambda (h t) h) l))
+  (case-list (lambda () (inr unit)) (lambda (h t) (inl h)) l))
 
 (define (rest l)
-  (case-list (lambda () 'ERROR) (lambda (h t) t) l))
+  (case-list (lambda () (inr unit)) (lambda (h t) (inl t)) l))
 
 (define (length l)
   (case-list (lambda () 0) (lambda (x y) (+ 1 (length y))) l))
 
 (define (nth-elmt l i)
-  'TODO)
+  (if (> i 0) (case-list (lambda () (inr unit)) (lambda (x y) (nth-elmt y (- i 1))) l) 'TODO))
 
 (define (map f l)
-  'TODO)
+  (case-list (lambda () '()) (lambda (x y) (link (f x) (map f y))) l))
 
 (define (reduce l f s)
   'TODO)
