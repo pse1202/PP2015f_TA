6e1dbe91f60074ddc32109516f013f359b6069d4
Sangeon Park <pse961202@gmail.com>
2015-11-11 16:56:51 +0900

    Fix H@ 5-3 comment

--- homeworks/hw5-skeleton/hw5-3.rkt
+++ homeworks/hw5-skeleton/hw5-3.rkt
@@ -10,7 +10,7 @@
   (equal? l empty) #|bool|#)
 
 (define (fst #|t list -> t + unit|# l #|t list|#)
-  (case-list #|unit -> t + unit * (t * t list -> t + unit) * t list -> t + unit|# (lambda (u) #|unit -> t + unit|# (inr unit)) (lambda (h t) #|t * t list -> t + unit|# (inl h)) #|t + unit|# l))
+  (case-list #|unit -> t + unit , (t , t list -> t + unit) , t list -> t + unit|# (lambda (u) #|unit -> t + unit|# (inr unit)) (lambda (h t) #|t , t list -> t + unit|# (inl h)) #|t + unit|# l))
 
 (define (rest #|t list -> t list + unit|# l)
   (case-list (lambda (u) (inr unit) #|t list + unit|#) (lambda (h #|t|# t #|t list|#) (inl t) #|t list + unit|#) l) #|t list + unit|#)
@@ -18,14 +18,14 @@
 (define (length #|t list -> int|# l)
   (case-list (lambda (u) 0) (lambda (x y) (+ 1 (length y))) l))
 
-(define (nth-elmt #|t list * int -> t + unit|# l i)
+(define (nth-elmt #|t list , int -> t + unit|# l i)
   (if (> i 0) #|bool|# (case-list (lambda (u) (inr unit) #|t + unit|#) (lambda (x #|t|# y #|t list|#) (nth-elmt y (- i 1)) #|t + unit|#) l)
       (if (= i 0) (fst l) #|t + unit|# (inr unit) #|t + unit|#)) #|t + unit|#)
 
-(define (map #|(t -> a) * t list -> a list|# f #|t -> a|# l #|t list|#)
+(define (map #|(t -> a) , t list -> a list|# f #|t -> a|# l #|t list|#)
   (case-list (lambda (u) empty #|a list|#) (lambda (x #|t|# y #|t list|#) (link (f x) #|a|#  (map f y) #|a list|#)) l) #|a list|#)
 
-(define (reduce #|t list * (t * a -> a) * a -> a|# l #|t list|# f #|t * a -> a|# s #|a|#)
+(define (reduce #|t list , (t , a -> a) , a -> a|# l #|t list|# f #|t , a -> a|# s #|a|#)
   (case-list (lambda (u #|unit|#) s #|a|#) (lambda (x #|t|# y #|t list|#) (reduce y  f (f x s) #|a|#)) l)#|a|#)
   
 
