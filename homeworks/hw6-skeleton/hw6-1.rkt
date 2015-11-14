8baf00a3c91f1eec02320fcc919d31993f71b758
Sangeon Park <pse961202@gmail.com>
2015-11-14 16:39:18 +0900

    HW 6-1 done

--- homeworks/hw6-skeleton/hw6-1.rkt
+++ homeworks/hw6-skeleton/hw6-1.rkt
@@ -3,6 +3,16 @@
 ;;; If these statements are omitted, your submission will be graded 0.
 (provide memo-ways)
 
-(define (memo-ways n m) ; memo-ways: int * int -> int
-  'TODO)
+(define (memoize fun)
+  (let ((results (make-hash)))
+    (lambda args
+      (when (not (hash-has-key? results args))
+        (hash-set! results args (apply fun args)))
+      (hash-ref results args))))
 
+(define memo-ways ; memo-ways: int * int -> int
+  (memoize (lambda (n k)
+             (cond ((= n 0) 1)
+                   ((= k 0) 1)
+                   (else (+ (memo-ways n (- k 1))
+                            (memo-ways (- n 1) k)))))))
